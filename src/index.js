67c8b57771bf4b8b3b00a96d364abfaf150bbc1e
summit3 <summitkhadka3@gmail.com>
2024-01-19 11:06:06 +0000

    Create Dynamic Styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-const name = "Summit"
+const root = ReactDOM.createRoot(document.getElementById("root"))
+const today = new Date()
+const timeHours = today.getHours()
+let message;
+let customStyle = {
+  color: ""
+}
 
-ReactDOM.render(<h2>Hello {name}</h2>,
-document.querySelector("#root"))
+if (timeHours<12) {
+  message = "Good Morning"
+  customStyle.color = "red"
+} else if (timeHours<18) {
+  message = "Good Afternoon"
+  customStyle.color = "green"
+} else if (timeHours<24) {
+  message = "Good Evening"
+  customStyle.color = "blue"
+}
+
+root.render(<h1 className='title' style={customStyle}>{message}</h1>)
